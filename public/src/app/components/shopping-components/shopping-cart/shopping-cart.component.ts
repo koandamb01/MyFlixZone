9458590bdee354f2a8a2da7a6aea4a37e21550b7
David Noh <davidnohj@gmail.com>
2018-11-02 08:21:24 -0700

    hello

--- public/src/app/components/shopping-components/shopping-cart/shopping-cart.component.ts
+++ public/src/app/components/shopping-components/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { User } from '../../../models/user';
+import { ShoppingService } from '../../../services/shopping.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -10,12 +13,19 @@ export class ShoppingCartComponent implements OnInit {
   showCart: boolean;
   showReviewOrder: boolean;
   showOrderDetail: boolean;
-  constructor() { }
+  constructor(
+    private shopService: ShoppingService,
+    private _router: Router
+  ) { }
+
+  total: any;
+  listOfItems: any[];
 
   ngOnInit() {
     this.showCart = true;
     this.showReviewOrder = false;
     this.showOrderDetail = false;
+    this.getCart();
   }
 
 
@@ -49,5 +59,20 @@ export class ShoppingCartComponent implements OnInit {
     this.showCart = false;
     this.showReviewOrder = false;
     this.showOrderDetail = true;
+  }
+
+
+  getCart(){
+    this.shopService.getCart().subscribe(res => {
+      if (res['status'] == false) {
+        console.log("Could not get cart");
+      }
+      else if (res['status'] == true) {
+        console.log(res);
+        this.total = res.orderDetail.order.total;
+        console.log(this.total);
+        this.listOfItems = res.orderDetail.details;
+      }
+    });
   }
 }
--- public/src/app/services/shopping.service.ts
+++ public/src/app/services/shopping.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
+import { map, filter, scan, catchError } from 'rxjs/operators';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingService {
+  private baseUrl: string = 'http://localhost:8080';
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private options = new RequestOptions({ headers: this.headers });
 
-  constructor() { }
+  constructor(private _http: Http) { }
+
+
+  getCart() {
+    return this._http.get(this.baseUrl + '/paypal/getCart', this.options)
+      .pipe
+      (
+      map((response: Response) => response.json()),
+      catchError(this.errorHandler)
+      )
+  }
+
+  errorHandler(error: Response) {
+    return Observable.throw(error || "Server Error Service Side")
+  }
 }
